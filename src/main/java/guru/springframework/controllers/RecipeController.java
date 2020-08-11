e87c05e6f0461fa6acfa5ed6df36f6642cf3a695
enjoycodingwithme <Hiba2010##>
2020-08-11 20:43:46 +0200

    Handling exception in reactive programming.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateInputException;
 //import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -81,19 +82,15 @@ public class RecipeController {
         return "redirect:/";
     }
 
-   /* @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception){
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Exception exception, Model model){
 
         log.error("Handling not found exception");
         log.error(exception.getMessage());
+        model.addAttribute("exception", exception);
 
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("404error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-    }*/
+        return "404error";
+    }
 
 }
